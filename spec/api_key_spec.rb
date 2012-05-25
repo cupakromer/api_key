00db6872fc3bd3603d03f95867ab9204df33a537
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-24 22:39:28 -0400

    Refactor test to define class variable.

--- spec/api_key_spec.rb
+++ spec/api_key_spec.rb
@@ -8,22 +8,21 @@ class APIKey
 end
 
 describe APIKey do
+  let( :apiKeyClass ) { APIKey }
+
   it "sets the default key to nil" do
-    UsingAPIKey = APIKey
-    UsingAPIKey.default_api_key.should be_nil
+    apiKeyClass.default_api_key.should be_nil
   end
 
   it "allows a default key to be set" do
-    UsingAPIKey = APIKey
-    UsingAPIKey.default_api_key = "a default key"
-    UsingAPIKey.default_api_key.should == "a default key"
+    apiKeyClass.default_api_key = "a default key"
+    apiKeyClass.default_api_key.should == "a default key"
   end
 
   it "retains only the last key set" do
-    UsingAPIKey = APIKey
-    UsingAPIKey.default_api_key = "a default key"
-    UsingAPIKey.default_api_key.should == "a default key"
-    UsingAPIKey.default_api_key = "new key set"
-    UsingAPIKey.default_api_key.should == "new key set"
+    apiKeyClass.default_api_key = "a default key"
+    apiKeyClass.default_api_key.should == "a default key"
+    apiKeyClass.default_api_key = "new key set"
+    apiKeyClass.default_api_key.should == "new key set"
   end
 end
