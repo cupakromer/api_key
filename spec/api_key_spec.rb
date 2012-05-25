a37afeec369cc23ee9707ab00ab30a2044509ef2
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-24 22:53:12 -0400

    Add context to test cases.

--- spec/api_key_spec.rb
+++ spec/api_key_spec.rb
@@ -10,19 +10,29 @@ end
 describe APIKey do
   let( :apiKeyClass ) { APIKey }
 
-  it "sets the default key to nil" do
-    apiKeyClass.default_api_key.should be_nil
+  context "for the Class" do
+    it "sets the default key to nil" do
+      apiKeyClass.default_api_key.should be_nil
+    end
+
+    describe "#default_api_key" do
+      it "allows a default key to be set" do
+        apiKeyClass.default_api_key = "a default key"
+        apiKeyClass.default_api_key.should == "a default key"
+      end
+
+      it "retains only the last default key set" do
+        apiKeyClass.default_api_key = "a default key"
+        apiKeyClass.default_api_key.should == "a default key"
+        apiKeyClass.default_api_key = "new key set"
+        apiKeyClass.default_api_key.should == "new key set"
+      end
+    end
   end
 
-  it "allows a default key to be set" do
-    apiKeyClass.default_api_key = "a default key"
-    apiKeyClass.default_api_key.should == "a default key"
-  end
-
-  it "retains only the last key set" do
-    apiKeyClass.default_api_key = "a default key"
-    apiKeyClass.default_api_key.should == "a default key"
-    apiKeyClass.default_api_key = "new key set"
-    apiKeyClass.default_api_key.should == "new key set"
+  context "as an instance" do
+    describe "#api_key" do
+      it "returns the class default key"
+    end
   end
 end
