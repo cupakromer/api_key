557ef53b7f04825b8e9c2fff45b7f4897b48280a
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-28 13:04:46 -0400

    [#30037205] Add spec for api_key_param_name.

--- spec/api_key_spec.rb
+++ spec/api_key_spec.rb
@@ -31,6 +31,24 @@ describe APIKey do
 
   let( :uses_api_key ) { apiKeyClass.new }
 
+  describe '#api_key_param_name' do
+    it "returns nil by default" do
+      uses_api_key.api_key_param_name.should be_nil
+    end
+
+    context "after the class calls .api_key_param_name" do
+      it "returns a symbol of the provided argument" do
+        [
+          ["a_string_key", :a_string_key],
+          [:a_symbol_key,  :a_symbol_key],
+        ].each do |param_name, param_name_as_sym|
+          apiKeyClass.class_eval{ api_key_param_name param_name }
+          uses_api_key.api_key_param_name.should == param_name_as_sym
+        end
+      end
+    end
+  end
+
   describe "#api_key=" do
     it "sets the instance key" do
       uses_api_key.api_key = @INSTANCE_KEY
