2fe066939d65bfcf3dfa1783389a091e1370650a
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-28 15:01:13 -0400

    [#30037731] Add file and module rdoc.

--- lib/api_key.rb
+++ lib/api_key.rb
@@ -1,3 +1,24 @@
+# Provides general support for web/REST classes working with API keys.
+#
+# Many different web API require the use of an API to interact with the
+# service. Some 3rd party Ruby APIs have implemented their own specific
+# solution. Some set the key on the Class, others allow for each instance
+# to set the key. This module provides mix-in support for a generalizd
+# signature for working with these types of APIs.
+#
+# Author::    Aaron Kromer (https://github.com/cupakromer)
+# Copyright:: Copyright (c) 2012
+# License::   Distributes under the same terms as Ruby
+
+# Mix-in module for providing support to work with API keys.
+#
+# Example
+#
+#   class XyzService
+#     include APIKey
+#
+#     api_key_param_name :service_key
+#   end
 module APIKey
   module ClassMethods
     attr_accessor :api_key
@@ -16,6 +37,8 @@ module APIKey
   attr_writer :api_key
 
   def api_key
+    # We are not lazy loading the @api_key in order to preserve the
+    # distinction between it and the default (class) key.
     @api_key || self.class.api_key
   end
 
