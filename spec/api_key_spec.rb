cff3d329d9e0deba167d7e8546298c8bc3c9b4ea
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-28 11:55:39 -0400

    Add more tests and update formatting.
    Re-adjust describe and context blocks to result in a `--format doc`
    reading more like an English spec. Also, add missing tests for the
    setter methods.

--- spec/api_key_spec.rb
+++ spec/api_key_spec.rb
@@ -6,53 +6,70 @@ end
 
 describe APIKey do
   before( :all ) { @DEFAULT_KEY = "a default key".freeze }
-
   let( :apiKeyClass ) { DummyClass.dup }
 
-  context "for the Class" do
-    it "sets the default key to nil" do
-      apiKeyClass.api_key.should be_nil
-    end
+  it "sets the default (class) key to nil" do
+    apiKeyClass.api_key.should be_nil
+  end
 
-    describe "#api_key" do
-      it "allows a default key to be set" do
-        apiKeyClass.api_key = @DEFAULT_KEY
-        apiKeyClass.api_key.should == @DEFAULT_KEY
-      end
-
-      it "retains only the last default key set" do
-        apiKeyClass.api_key = @DEFAULT_KEY
-        apiKeyClass.api_key.should == @DEFAULT_KEY
-        apiKeyClass.api_key = "new key set"
-        apiKeyClass.api_key.should == "new key set"
-      end
+  describe '.api_key=' do
+    it "allows a default key to be set" do
+      apiKeyClass.api_key = @DEFAULT_KEY
+      apiKeyClass.instance_eval{ @api_key }.should == @DEFAULT_KEY
     end
   end
 
-  context "as an instance" do
-    let( :uses_api_key ) { apiKeyClass.new }
+  describe ".api_key" do
+    it "retains only the last default key set" do
+      [ @DEFAULT_KEY, 'another key', 'new key set' ].each do |key|
+        apiKeyClass.api_key = key
+      end
 
-    describe "#api_key" do
-      context "no instance key set" do
-        it "returns nil when no default key set" do
+      apiKeyClass.api_key.should == "new key set"
+    end
+  end
+
+  let( :uses_api_key ) { apiKeyClass.new }
+
+  describe "#api_key=" do
+    it "sets the instance key" do
+      uses_api_key.api_key = @INSTANCE_KEY
+      uses_api_key.instance_eval{ @api_key }.should == @INSTANCE_KEY
+    end
+  end
+
+  describe "#api_key" do
+    context "given no instance key set" do
+      context "when no default (class) key set" do
+        it "returns nil" do
           uses_api_key.api_key.should be_nil
-        end
-
-        it "returns the default key" do
-          apiKeyClass.api_key = @DEFAULT_KEY
-          uses_api_key.api_key.should == @DEFAULT_KEY
         end
       end
 
-      context "instance key set" do
-        before( :all  ) { @INSTANCE_KEY = "an instance key".freeze }
-        before( :each ) { uses_api_key.api_key = @INSTANCE_KEY     }
+      context "when default (class) key set" do
+        it "returns the last default (class) key set" do
+          [ @DEFAULT_KEY, 'another key', 'new key set' ].each do |key|
+            apiKeyClass.api_key = key
+          end
 
-        it "returns the instance key when no default key set" do
+          uses_api_key.api_key.should == 'new key set'
+        end
+      end
+    end
+
+    context "given instance key set" do
+      before( :all  ) { @INSTANCE_KEY = "an instance key".freeze }
+      before( :each ) { uses_api_key.api_key = @INSTANCE_KEY     }
+
+      context "when no default (class) key set" do
+        it "returns instance key" do
+          uses_api_key.class.instance_eval{ @api_key }.should be_nil
           uses_api_key.api_key.should == @INSTANCE_KEY
         end
+      end
 
-        it "returns the instance key when default key set" do
+      context "when default (class) key set" do
+        it "returns instance key" do
           apiKeyClass.api_key = @DEFAULT_KEY
           uses_api_key.api_key.should == @INSTANCE_KEY
         end
